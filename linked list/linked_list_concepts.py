ba9b598f0b1c2df7c1c42c921f97a52574920960
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2022-09-12 00:57:04 +0200

    Updated the concepts to learn

---
+++ linked list/linked_list_concepts.py
@@ -0,0 +1,11 @@
+"""
+Singly Linked list concepts
+Doubly Linked list concepts
+Implementation of Simple linked list using 'Node'  classes
+Traversing a singly linked list and print each Node's value
+Create a singly linked list - Basic
+Create a single linked list  - Better
+Implement a DoublyNode and Doubly Linked List with append and to_list methods
+Prepend, Append, Insert, Size, Remove in Singly linked list
+Reverse a Singly linked list
+"""

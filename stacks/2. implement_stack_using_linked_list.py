fb7da639653ae30260f89b952b898029470534ca
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2022-10-01 18:30:24 +0200

    Added a new problem

---
+++ stacks/2. implement_stack_using_linked_list.py
@@ -0,0 +1,10 @@
+# Stack is a data structure which is helpful to manipulate or operate on the last element which was added
+# In Python, Stack is implemented with the help of list. Here, we implement with the help of linked list
+# In Stack, Last element which was added; is the first element which can be removed.
+
+# Concepts
+
+# Create a Stack class with some constant length and same values
+# Keep the total number of elements , last_index used in the Stack
+# Push the new element to the next available index
+# Remove the element from the last index

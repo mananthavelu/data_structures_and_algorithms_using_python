40daac5eba7481e940ef9b41328f0a36e529cbaf
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2022-10-09 12:41:34 +0200

    Updated with more problems and solutions

---
+++ recursion/6. list_permutation.py
@@ -0,0 +1,2 @@
+# Goal: Given a list,return all the permutations of the list
+
---
+++ trees/1. tree_concepts.py
@@ -0,0 +1,17 @@
+# Tree Concepts
+
+# Trees are non-linear data structure
+# Tree has a 'root' 
+# The access to the Tree is using 'root' 
+# Tree must be connected
+# Tree must not have cycles
+# Terms: 
+# Level: 'root' is at level 1 and the 'node' connected to it is at level 2 (Starts from top-> reaching bottom)
+# Ancestor/Descendant ; Parent/Child
+# Leaf: The nodes which dont have any child are leaf nodes or external nodes
+# Leaf: The nodes which have the children are 'internal nodes'
+# Height: The number of edges connecting two nodes from its bottom (starting at 0)
+# Height: Height of the tree = Height of the root node
+# Depth: Depth of the node is the total number of edges to its root
+# Height and Depth moves inveresely
+# 

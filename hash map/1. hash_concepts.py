d0ddd0ce60132319a8bdd58d2b327b4a6e12369c
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2022-10-11 19:24:13 +0200

    Updated

---
+++ hash map/1. hash_concepts.py
@@ -0,0 +1,2 @@
+# Hash tables / maps are efficient to saerch for an element. Constant time operation
+# Issues:  Hash function may be expensive, Collision may happen
---
+++ hash map/2. implementation_of_hash_map.py
@@ -0,0 +1,5 @@
+# What we need: a Hash function which converts the given input to a certain value which will act as an array index
+"""
+
+
+"""

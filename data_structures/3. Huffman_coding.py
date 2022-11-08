c2da16ffcbb9cd5f10188413dd1605b6f4c18d35
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2022-11-08 21:13:20 +0100

    Add new problem

---
+++ data_structures/3. Huffman_coding.py
@@ -0,0 +1,34 @@
+import sys
+
+def huffman_encoding(data):
+    pass
+
+def huffman_decoding(data,tree):
+    pass
+
+if __name__ == "__main__":
+    codes = {}
+
+    a_great_sentence = "The bird is the word"
+
+    print ("The size of the data is: {}\n".format(sys.getsizeof(a_great_sentence)))
+    print ("The content of the data is: {}\n".format(a_great_sentence))
+
+    encoded_data, tree = huffman_encoding(a_great_sentence)
+
+    print ("The size of the encoded data is: {}\n".format(sys.getsizeof(int(encoded_data, base=2))))
+    print ("The content of the encoded data is: {}\n".format(encoded_data))
+
+    decoded_data = huffman_decoding(encoded_data, tree)
+
+    print ("The size of the decoded data is: {}\n".format(sys.getsizeof(decoded_data)))
+    print ("The content of the encoded data is: {}\n".format(decoded_data))
+
+# Add your own test cases: include at least three test cases
+# and two of them must include edge cases, such as null, empty or very large values
+
+# Test Case 1
+
+# Test Case 2
+
+# Test Case 3

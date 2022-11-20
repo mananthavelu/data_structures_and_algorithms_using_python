c2373550eeaea9bdfe27001510cec343b7d0ae57
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2022-11-20 15:56:16 +0100

    Solve Problem 1

--- Basic algorithms/1. Square root.py
+++ Basic algorithms/1. Square root.py
@@ -8,6 +8,8 @@ def sqrt(number):
     Returns:
        int: Floored Square Root
    """
+   if number is None or '':
+      return None
    #candidate_value = number % 2
 
    # THe approach is similar to 'searching in Binary search tree'
@@ -15,18 +17,22 @@ def sqrt(number):
    return search_square_root(0, number, number)
 
 def search_square_root(left, right, number):
+   if left == right:
+      return left
    if left <= right:
-      middle_value = (left +right) % 2
-      if (middle_value*middle_value <= number) and ((middle_value + 1) * (middle_value + 1) > number):
-         return middle_value
-      elif (middle_value *middle_value) < number:
-         search_square_root(middle_value + 1, right, number)
+      middle_value = (left + ((right - left) // 2))
+      middle_value_squared = int(middle_value **2)
+      if middle_value_squared < number:
+         return search_square_root(middle_value + 1, right, number)
+      elif middle_value_squared > number:
+         return search_square_root(left, middle_value - 1, number)
       else:
-         search_square_root(left, middle_value - 1, number)
-   return left
+         return middle_value
+   return None
 
-print ("Pass" if  (3 == sqrt(9)) else "Fail")
-print ("Pass" if  (0 == sqrt(0)) else "Fail")
-print ("Pass" if  (4 == sqrt(16)) else "Fail")
-print ("Pass" if  (1 == sqrt(1)) else "Fail")
-print ("Pass" if  (5 == sqrt(27)) else "Fail")
+print ("Pass" if  (3 == sqrt(9)) else "Fail")#returns Pass
+print ("Pass" if  (0 == sqrt(0)) else "Fail")#returns Pass
+print ("Pass" if  (4 == sqrt(16)) else "Fail")#returns Pass
+print ("Pass" if  (1 == sqrt(1)) else "Fail")#returns Pass
+print ("Pass" if  (5 == sqrt(27)) else "Fail")#returns Pass
+print ("Pass" if  (None == sqrt(None)) else "Fail")#returns Pass

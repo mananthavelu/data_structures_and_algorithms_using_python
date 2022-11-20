67c90c024daace0bdd7d07bca96a9dc47b3c12da
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2022-11-20 22:38:47 +0100

    Update problem 2

--- Basic algorithms/2. Search in a rotated array.py
+++ Basic algorithms/2. Search in a rotated array.py
@@ -10,15 +10,32 @@ def rotated_array_search(input_list, number):
     # Find the index of a random pivot
     left = 0
     right = len(input_list) - 1
-    while left <= right:
+    def find_pivot(input_list, left, right):
+        if left == right:
+            return left
+        if (right - left) == 1:
+            return right
+        if (left - right) == 1:
+            return left
         middle = (left + right) // 2
-        if input_list[middle] < (input_list[middle - 1]) and (input_list[middle] < input_list[middle + 1]):
-            pivot = middle
-            break
-        elif input_list[middle] > (input_list[middle - 1]) and (input_list[middle]) < input_list[middle + 1]:
+        if input_list[middle] > input_list[right]:
+            return find_pivot(input_list, middle, right)
+        elif input_list[middle] < input_list[right]:
+            return find_pivot(input_list, left, middle)
+        else:
+            return middle
+    pivot = find_pivot(input_list, left, right)
 
-        
-   pass
+    def binary_search(input_list, number, left, right, pivot):
+        if number == input_list[pivot]:
+            return pivot
+        elif input_list[pivot] <= number and number < input_list[right]:
+            return binary_search(input_list, number, ((right - input_list[pivot])//2) + pivot, pivot, right)
+        elif input_list[0] <= number and number <= input_list[pivot - 1]:
+            return binary_search(input_list, number, ((pivot - left)//2), 0, pivot-1)
+        else:
+            return -1
+    return binary_search(input_list, number, 0, right, pivot)
 
 def linear_search(input_list, number):
     for index, element in enumerate(input_list):

79087c5099478880d66df40cbfcc4ece9a59d501
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2023-02-13 14:31:17 +0100

    Add test cases

--- 1_Basic algorithms/problem_7.py
+++ 1_Basic algorithms/problem_7.py
@@ -42,3 +42,37 @@ class RouteTrie:
 Trie_example = RouteTrie()
 print(RouteTrie())
 """
+
+
+# A RouteTrieNode will be similar to our autocomplete TrieNode... with one additional element, a handler.
+class RouteTrieNode:
+    def __init__(self, value=""):
+        # Initialize the node with children as before, plus a handler
+        self.value = value
+        self.children = {}
+        self.handler = None
+
+    def insert(self, next_path, handler=None):
+        # Insert the node as before
+        self.children[next_path] = self.children.get(next_path, RouteTrieNode(next_path))
+        self.children[next_path].handler = handler
+
+
+#Test cases
+
+
+# Here are some test cases and expected outputs you can use to test your implementation
+
+# create the router and add a route
+router = Router("root handler", "not found handler")  # remove the 'not found handler' if you did not implement this
+router.add_handler("/home/about", "about handler")  # add a route
+router.add_handler("/home/about/team", "about team")  # add a route
+
+
+# some lookups with the expected output
+print(router.lookup("/"))  # should print 'root handler'
+print(router.lookup("/home"))  # should print 'not found handler' or None if you did not implement one
+print(router.lookup("/home/about"))  # should print 'about handler'
+print(router.lookup("/home/about/"))  # should print 'about handler' or None if you did not handle trailing slashes
+print(router.lookup("/home/about/me"))  # should print 'not found handler' or None if you did not implement one
+print(router.lookup("/home/about/team"))

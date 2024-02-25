4c25a40a19c2e5866c0b4afe301dc571a786741e
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2024-02-25 16:19:56 +0100

    Refresh Linked lists

--- 0_Data_structures/2_linked list/2. singly_linked_list.py
+++ 0_Data_structures/2_linked list/2. singly_linked_list.py
@@ -3,17 +3,191 @@
 # Linkedlist has a node
 # Node stores the data we want to store
 # Each node has its link to its 'next' node, thus creating a sequence with links ('link'ed 'list')
+class Node:
+    def __init__(self, data = None):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self, head = None):
+        self.head = head
+
+    def is_empty(self):
+        return self.head is None
+
+    def append(self, data):
+        # Add as a head if it does not exist
+        if self.head is None:
+            self.head = Node(data)
+            return            
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        current_node.next = Node(data)                                                                                                                                                                                                                                                                                                                      
+        return
+    
+    def size(self):
+        if self.is_empty():
+            return 0
+        current_node = self.head
+        current_size = 0
+        while current_node:
+            current_size += 1
+            current_node = current_node.next
+        return current_size
+    
+    def convert_list_to_ll(self, input_list):
+        if self.head is None:
+            self.head = Node(input_list[0])
+            current_node = self.head
+            for element in input_list[1:]:
+                current_node.next = Node(element)
+                current_node = current_node.next
+        else:
+            current_node = self.return_end_node()
+            for element in input_list:
+                current_node.next = Node(element)
+                current_node = current_node.next
+        return
+
+    def return_end_node(self):
+        current_node = self.head
+        while current_node.next:
+            current_node = current_node.next
+        return current_node
+
+    def prepend(self, data):
+        if self.head is None:
+            self.head = Node(data)
+            return
+        #current_node = self.head
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+        print(f"prepended {new_node.data} and the head is {self.head.data}")
+        return
+
+    def reverse_linked_list(self):
+        current_node = self.head
+        print(current_node.data)
+        reversed_linked_list = SinglyLinkedList()
+        reversed_linked_list.append(current_node.data)
+        print(f"Here I am {reversed_linked_list}")
+        while current_node.next:
+            node_to_prepend = current_node.next
+            reversed_linked_list.prepend(node_to_prepend.data)
+            current_node = current_node.next
+        return reversed_linked_list
+
+    def add_node_at_position(self, data, position):
+        if position == 1:
+            self.prepend(data)
+            return
+        current_position = 1
+        current_node = self.head
+        new_node = Node(data)
+        while current_node:
+            if (current_position + 1) == position:
+                new_node.next = current_node.next
+                current_node.next = new_node
+                return
+            current_position += 1
+            current_node = current_node.next
+        return
+    
+    def remove_head(self):
+        if self.head is None:
+            return
+        head_to_be_node = self.head.next
+        self.head = head_to_be_node
+        return
+
+    def remove_a_node_with_value(self, data):
+        if self.head.data == data:
+            self.remove_head()
+        current_node = self.head
+        while current_node.next:
+            if current_node.next.data == data:
+                current_node.next = current_node.next.next
+                return
+            current_node = current_node.next
+
+    def __repr__(self):
+        result_string = ''
+        current_node = self.head
+        while current_node:
+            result_string += current_node.data
+            result_string += '->'
+            current_node = current_node.next
+        return result_string[:-2]
+
+example_ll = SinglyLinkedList()
+example_ll.convert_list_to_ll(['Germany',"Austria","Belgium","Netherlands"])
+print(example_ll)
+example_ll.add_node_at_position('Switzerland', 1)
+print(example_ll)
+example_ll.remove_a_node_with_value('Netherlands')
+print(example_ll)
+#print("Here")
+#example_ll.reverse_linked_list()
+#print(f"Reversed list is {reversed_list}")
+
+class NodeHeadTail:
+    def __init__(self, data = None):
+        self.data = data
+        self.next = None
+
+class SinglyLinkedListHeadTail:
+    def __init__(self, head = None, tail = None):
+        self.head = head
+        self.tail = tail
+
+    def __repr__(self):
+        result = ""
+        current_node = self.head
+        while current_node.next:
+            result += current_node.data
+            result += "->"
+            current_node = current_node.next
+        return result[:-2]
+
+    def append(self,data):
+        if self.head is None:
+            self.head = Node(data)
+            self.tail = self.head
+
+        new_node = Node(data)
+        self.tail.next = new_node
+        self.tail = self.tail.next
+        
+new_ss_list = SinglyLinkedListHeadTail()
+new_ss_list.append("C1")
+new_ss_list.append("C2")
+new_ss_list.append("C3")
+new_ss_list.append("C4")
+print(f"The new ss list is: {new_ss_list}")
+
+"""
+new_node = Node('Switezerland')
+new_linked_list = SinglyLinkedList(new_node)
+print(new_linked_list.size())
+new_linked_list.append('Zurich')    
+print(new_linked_list.size())
+print(new_linked_list.is_empty())
+print(new_linked_list)
+new_linked_list.convert_list_to_ll(['Germany',"Austria"])
+print(new_linked_list)       
+
 
 class Node:
     def __init__(self, data = None):
-        """Constructor for the class
+        #Constructor for the class
 
-        Args:
-            data (_type_, optional): Content of the Node. Defaults to None.
-        """
+        #Args:
+        #    data (_type_, optional): Content of the Node. Defaults to None.
+        
         self.data = data
         self.next = None
-        
 # Linked list has head and tail initialized to 'None'
 class SinglyLinkedList:
     # Initialization
@@ -103,7 +277,7 @@ class SinglyLinkedList:
             current_node = current_node.next
         return False
     # Reverse a linked list
-    """     
+        
     def reverse_linked_list(self):
         # Keep the None as the self.heads next
         # 
@@ -115,7 +289,6 @@ class SinglyLinkedList:
             current_node = current_node.next
         self.head = node_to_point_to
         return 
-    """
 
     # convert linked list to list
     def to_list(self):
@@ -160,8 +333,9 @@ print(linked_list_one)# Head node is given an entry point
 linked_list_one.remove_node_at_position(2)
 print(linked_list_one)# Head node is given an entry point
 print(linked_list_one.search(2))
-""" linked_list_one.reverse_linked_list()
-print(linked_list_one) """
+linked_list_one.reverse_linked_list()
+print(linked_list_one)
 # TODO Search for a value,pop a value, Size of a linked list
 print(linked_list_one.head.data)
 print(linked_list_one.to_list())
+"""
--- 0_Data_structures/2_linked list/3. doubly_linked_list.py
+++ 0_Data_structures/2_linked list/3. doubly_linked_list.py
@@ -16,12 +16,66 @@ class DoublyLinkedList:
         self.head = head
         self.tail = tail
         
-    def add_node(new_data):
+    def append(self, data):
         if self.head is None:
-            self.head = DoublyNode(new_data)
-            self.tail = self.Head
+            self.head = DoublyNode(data)
+            self.tail = self.head
+            return
         
-        self.tail.next = DoublyNode(new_data)
+        self.tail.next = DoublyNode(data)
         self.tail.next.previous = self.tail
         self.tail = self.tail.next
         return
+
+    def append_at_position(self, data, position):
+        if position == 1:
+            self.head = DoublyNode(data)
+            self.tail = self.head
+            return
+        current_position = 1
+        current_node = self.head
+        new_node = DoublyNode(data)
+        while current_node:
+            if (current_position + 1) == position:
+                new_node.next = current_node.next
+                current_node.next.previous = new_node
+                current_node.next = new_node
+                new_node.previous = current_node
+            current_node = current_node.next
+            current_position += 1
+        return
+
+    def delete_element_at_position(self, position):
+        if position == 1:
+            self.head = None
+            self.tail = None
+            return
+        current_position = 1
+        current_node = self.head
+        while current_node:
+            if (current_position + 1) == position:
+                current_node.next.next.previous == current_node
+                current_node.next =current_node.next.next
+                return
+            current_node = current_node.next
+            current_position += 1
+
+    def __repr__(self):
+        result = ""
+        current_node = self.head
+        while current_node:
+            result += current_node.data
+            result += "->"
+            current_node = current_node.next
+        return result[:-2]
+
+new_dd_list = DoublyLinkedList()
+new_dd_list.append("C1")
+new_dd_list.append("C2")
+new_dd_list.append("C3")
+new_dd_list.append("C4")
+new_dd_list.append("C5")
+new_dd_list.append_at_position("C3A", 2)
+print(new_dd_list)
+new_dd_list.delete_element_at_position(4)
+print(new_dd_list)

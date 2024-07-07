85de594cb53711271b936ed6c3100380191f9de4
Marimuthu Ananthavelu <mari.a.muthu@gmail.com>
2024-07-07 21:49:30 +0200

    Update the hash map

--- 1_Basic algorithms/6_search/0. search_a_target_in_array.py
+++ 1_Basic algorithms/6_search/0. search_a_target_in_array.py
@@ -13,24 +13,3 @@ def binary_search(input_array, target):
             return True
     return False
 print(binary_search([1,2,3,4,5], 4))
-
-""" 
-# Recursive solution
-
-def binary_search_recursive(array, target):
-    return binary_search_helper(array, target, 0, len(array) - 1)
-
-def binary_search_helper(array, target, start_index, end_index):
-    if start_index > end_index:
-        return -1
-    middle_index = (start_index + end_index) // 2
-    
-    if array[middle_index] == target:
-        return middle_index
-    elif array[middle_index] > target:
-        binary_search_helper(array, target, start_index, middle_index - 1)
-    else:
-        binary_search_helper(array, target, middle_index + 1, end_index)
-
-print(binary_search_recursive([1,2,3,4,5,5], 5))
-"""
--- 1_Basic algorithms/6_search/1. search_a_target_in_array_variation.py
+++ 1_Basic algorithms/6_search/1. search_a_target_in_array_variation.py
@@ -1,20 +1,3 @@
-# Interative solution
-"""
-def binary_search(input_array, target):
-    start_index = 0
-    end_index = len(input_array) - 1
-    while start_index <= end_index:
-        middle_index = (start_index + end_index) // 2
-        if input_array[middle_index] > target:
-            end_index = middle_index - 1
-        elif input_array[middle_index] < target:
-            start_index = middle_index + 1
-        else:
-            return middle_index
-    return -1
-    
-print(binary_search([1,2,3,4,5], 4))
-""" 
 # Recursive solution
 # Goal: Binary search tree variation - Find the first occurance of the element in an ordered array
 def binary_search_recursive(array, target):
